f4f01cb04836925a959320a774b54a497b63f53a
Ryan-Zayne <phoenixastro97@gmail.com>
2022-12-26 23:16:30 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
-import './App.css';
 
 function App() {
 	return (
