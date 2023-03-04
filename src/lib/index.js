61c72165617387df38d701e10bb4b4a762c4ce07
Ryan-Zayne <phoenixastro97@gmail.com>
2023-03-04 15:22:52 +0100

    Practicing merge resolving

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,20 +1,17 @@
-import earth from '../assets/mobile/image-deep-earth.jpg';
-import curiosity from '../assets/mobile/image-curiosity.jpg';
-import fisheye from '../assets/mobile/image-fisheye.jpg';
-import fromAbove from '../assets/mobile/image-from-above.jpg';
-import nightArcade from '../assets/mobile/image-night-arcade.jpg';
-import soccer from '../assets/mobile/image-soccer-team.jpg';
-import grid from '../assets/mobile/image-grid.jpg';
-import pocket from '../assets/mobile/image-pocket-borealis.jpg';
+export { default as earth } from '../assets/mobile/image-deep-earth.jpg';
+export { default as curiosity } from '../assets/mobile/image-curiosity.jpg';
+export { default as fisheye } from '../assets/mobile/image-fisheye.jpg';
+export { default as fromAbove } from '../assets/mobile/image-from-above.jpg';
+export { default as nightArcade } from '../assets/mobile/image-night-arcade.jpg';
+export { default as soccer } from '../assets/mobile/image-soccer-team.jpg';
+export { default as grid } from '../assets/mobile/image-grid.jpg';
+export { default as pocket } from '../assets/mobile/image-pocket-borealis.jpg';
 
-import earth2 from '../assets/desktop/image-deep-earth.jpg';
-import curiosity2 from '../assets/desktop/image-curiosity.jpg';
-import fisheye2 from '../assets/desktop/image-fisheye.jpg';
-import fromAbove2 from '../assets/desktop/image-from-above.jpg';
-import nightArcade2 from '../assets/desktop/image-night-arcade.jpg';
-import soccer2 from '../assets/desktop/image-soccer-team.jpg';
-import grid2 from '../assets/desktop/image-grid.jpg';
-import pocket2 from '../assets/desktop/image-pocket-borealis.jpg';
-
-export { earth, curiosity, fisheye, fromAbove, nightArcade, soccer, grid, pocket };
-export { earth2, curiosity2, fisheye2, fromAbove2, nightArcade2, soccer2, grid2, pocket2 };
+export { default as earth2 } from '../assets/desktop/image-deep-earth.jpg';
+export { default as curiosity2 } from '../assets/desktop/image-curiosity.jpg';
+export { default as fisheye2 } from '../assets/desktop/image-fisheye.jpg';
+export { default as fromAbove2 } from '../assets/desktop/image-from-above.jpg';
+export { default as nightArcade2 } from '../assets/desktop/image-night-arcade.jpg';
+export { default as soccer2 } from '../assets/desktop/image-soccer-team.jpg';
+export { default as grid2 } from '../assets/desktop/image-grid.jpg';
+export { default as pocket2 } from '../assets/desktop/image-pocket-borealis.jpg';
